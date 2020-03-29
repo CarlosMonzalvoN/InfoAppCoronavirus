1548aa9752ae5f5f7e50411b407e009f42a7394b
CarlosMonzalvoN <carlos.monzalvo.n@gmail.com>
2020-03-29 17:53:26 -0600

    initial commit

---
+++ InfoAppCoronavirus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfoAppCoronavirus
+//
+//  Created by Pedro Carlos Monzalvo Navarro on 29/03/20.
+//  Copyright © 2020 Pedro Carlos Monzalvo Navarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
